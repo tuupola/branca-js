aa0b17dac00b26787605e7c7d57a2d14f269cdb9
Mika Tuupola <tuupola@appelsiini.net>
2017-07-22 21:56:41 +0300

    Test with tape and Travis

--- test.js
+++ test.js
@@ -1,12 +1,15 @@
 "use strict";
 
+const test = require("tape");
 const branca = require("./branca")("supersecretkeyyoushouldnotcommit");
+const nonce = Buffer.from([0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c]);
 
-let nonce = Buffer.from([0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c]);
-let timestamp = 123206400;
+test("test vector 1", function (tape) {
+    tape.plan(2);
 
-let token = branca.encode("Hello world!", timestamp, nonce);
-console.log(token);
+    let token = branca.encode("Hello world!", 123206400, nonce);
+    tape.equal(token, "4si6Rr26CjfyVydzEiKBwuwkQwvjQhmBHTKSXAyHGcaFYA5kEp45XR1Amgblh");
 
-let payload = branca.decode(token);
-console.log(payload);
+    let message = branca.decode(token);
+    tape.equal(message, "Hello world!");
+});
